e01545610cd90519144ec005f284efa1ea0bfa76
up202108832 <up202108832@up.pt>
2023-12-21 18:33:15 +0000

    us

--- resources/views/pages/about_us.blade.php
+++ resources/views/pages/about_us.blade.php
@@ -7,6 +7,6 @@
         <h2 class="text-center">About <span>Us</span></h2>
         <p>The primary focus of ShowsMe is to provide an accessible and user-friendly platform that empowers event organizers to effortlessly promote and showcase their events to a global audience. This website is designed to enhance the experience for every user by simplifying the process of creating events, as well as discovering and purchasing tickets, all in one place.</p>
         <h2 class="text-center">Contact <span>Us</span></h2>
-        <p>Contact US at showsme@gmail.com</p>
+        <p>Contact us at showsme@gmail.com</p>
     </section>
 @endsection
